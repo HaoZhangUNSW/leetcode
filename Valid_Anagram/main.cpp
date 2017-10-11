4deb721737f3040063438e4ddc5b2989609fa943
zhpmatrix <zhpmatrix@gmail.com>
2017-10-11 15:53:59 +0800

    Changed

--- Valid_Anagram/main.cpp
+++ Valid_Anagram/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
-bool isAnagram(string s, string t){
-	return false;
+
+bool isAnagram0(string s, string t){
+	if(s.length() != t.length() ){return false;}
+	
+	unordered_map<char, int> _counts;
+	int n = s.length();
+	for(int i = 0;i < n;i++){
+		_counts[s[i]]++;
+		_counts[t[i]]--;
+	}
+	for(auto& _count:_counts){
+		if(_count.second){return false;}
+	}		
+	return true;
 }
+
+bool isAnagram(string s, string t){
+	if(s.length() != t.length()){return false;}
+	int n = s.length();
+	int counts[26] = {0};
+	for(int i = 0;i < n;i++){
+		counts[s[i] - 'a']++;
+		counts[t[i] - 'a']--;
+	}
+	for(int i = 0;i < 26;i++){
+		if(counts[i]){return false;}
+	}
+	return true;
+}
+
 int main(){
+	cout << isAnagram("car", "acr") << endl;
 	return 0;
 }
