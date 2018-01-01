8c7c2401b58c564423d94bf99dc1afdb01468781
zhpmatrix <zhpmatrix@gmail.com>
2018-01-01 14:27:22 +0800

    add readme

--- Remove_K_Digits/main.cpp
+++ Remove_K_Digits/main.cpp
@@ -3,9 +3,22 @@
 #include <vector>
 using namespace std;
 
+string getRst(vector<char>& stk){
+	int idx = 0;
+	int size = stk.size();
+	while(idx < size && stk[idx] == '0'){idx++;}
+	if(idx == size){return "0";}
+	else{
+		string rst = "";
+		for(int i = idx;i < size;i++){
+			rst += stk[i];
+		}
+		return rst;
+	}
+}
+
 string removeKdigits(string& nums, int k){
 	int oldNumsLen = nums.length();
-	int newNumsLen = oldNumsLen - k;
 	vector<char> stk;
 	int top = 0;	
 	for(int i = 0;i < oldNumsLen;i++){
@@ -18,21 +31,13 @@ string removeKdigits(string& nums, int k){
 		stk.push_back(curNum);
 		top++;
 	}
-	int idx = 0;
-	while(idx < newNumsLen && stk[idx] == '0'){idx++;}
-	if(idx == newNumsLen){return "0";}
-	else{
-		string rst = "";
-		for(int i = idx;i < newNumsLen;i++){
-			rst += stk[i];
-		}
-		return rst;
-	}
+	return getRst(stk);
 }
 
 int main(){
-	string nums = "10";
+	string nums = "145637892";
+	string nums1 = "10";
 	int k = 2;
-	cout << removeKdigits(nums, k) << endl;
+	cout << removeKdigits(nums1, k) << endl;
 	return 0;
 }
